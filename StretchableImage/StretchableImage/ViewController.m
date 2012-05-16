70641f7deb3c83445fdac01ece552dca5c6aee86
Leonardo Parro Jr <jayrparro@yahoo.com>
2012-05-16 18:29:00 +0800

    added image stretch w/o tiled

--- StretchableImage/StretchableImage/ViewController.m
+++ StretchableImage/StretchableImage/ViewController.m
@@ -22,17 +22,23 @@
 {
     [super viewDidLoad];
     
-    // An image with caps stretched
+    // An image stretched    
+    UIImage* image0 = [UIImage imageNamed:FILE_IMG_BLUE_HEADER];
+    UIImageView* imageView = [[UIImageView alloc] initWithImage:image0];
+    imageView.frame = CGRectMake(20, 40, 150.0, image0.size.height);
+    [self.view addSubview:imageView];
+    
+    // An image with caps stretched    
     // note- stretchableImageWithLeftCapWidth deprecated in iOS 5
     UIImage* image1 = [[UIImage imageNamed:FILE_IMG_BLUE_HEADER] stretchableImageWithLeftCapWidth:6.0 topCapHeight:0.0];
     UIImageView* imageView1 = [[UIImageView alloc] initWithImage:image1];
-    imageView1.frame = CGRectMake(50, 50, 100.0, image1.size.height);
+    imageView1.frame = CGRectMake(20, 115, 100.0, image1.size.height);
     [self.view addSubview:imageView1];
 
     // note- resizableImageWithCapInsets use in iOS 5+    
     UIImage *image2 = [[UIImage imageNamed:FILE_IMG_GRAY_FRAME] resizableImageWithCapInsets:UIEdgeInsetsMake(6.0, 6.0, 6.0, 6.0)];
     UIImageView* imageView2 = [[UIImageView alloc] initWithImage:image2];
-     imageView2.frame = CGRectMake(50, 100, 200.0, 200);
+     imageView2.frame = CGRectMake(20, 170, 200.0, 200);
      [self.view addSubview:imageView2];
     
     self.imageView3.image = [[UIImage imageNamed:FILE_IMG_BLUE_HEADER] resizableImageWithCapInsets:UIEdgeInsetsMake(0.0, 6.0, 0.0, 6.0)];
